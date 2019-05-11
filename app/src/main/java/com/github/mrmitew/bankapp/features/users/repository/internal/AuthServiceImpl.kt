31a67c59c2d3a4a13c98e6c41df6a9a111227d47
Stefan Mitev <mr.mitew@gmail.com>
2019-05-11 12:16:51 +0200

    [add] Use inflight request caching for when a user token is fetched

--- app/src/main/java/com/github/mrmitew/bankapp/features/users/repository/internal/AuthServiceImpl.kt
+++ app/src/main/java/com/github/mrmitew/bankapp/features/users/repository/internal/AuthServiceImpl.kt
@@ -1,12 +1,25 @@
 package com.github.mrmitew.bankapp.features.users.repository.internal
 
+import com.github.mrmitew.bankapp.features.common.usecase.UseCase
+import com.github.mrmitew.bankapp.features.common.usecase.reuseInflight
 import com.github.mrmitew.bankapp.features.users.repository.AuthService
 import com.github.mrmitew.bankapp.features.users.repository.RemoteUserRepository
 
-class AuthServiceImpl(private val remoteUserRepository: RemoteUserRepository) :
+class AuthServiceImpl(remoteUserRepository: RemoteUserRepository) :
     AuthService {
+
+    private class GetOrFetchUserTokenUseCase(private val remoteUserRepository: RemoteUserRepository) :
+        UseCase<String, String> {
+        private val internalUseCase = object : UseCase<String, String> {
+            override suspend fun invoke(param: String): String = remoteUserRepository.getUserToken(param)
+        }.reuseInflight()
+
+        override suspend fun invoke(param: String): String = internalUseCase(param)!!
+    }
+
+    private val getOrFetchUserToken = GetOrFetchUserTokenUseCase(remoteUserRepository)
+
     override suspend fun getUserToken(appToken: String): String {
-        // TODO: Implement inflight request caching
-        return remoteUserRepository.getUserToken(appToken)
+        return getOrFetchUserToken(appToken)
     }
 }
